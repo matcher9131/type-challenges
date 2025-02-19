b9976dcccf76b157a6ff2c4589e04cc5b11e3fc5
matcher9131 <matcher9131cd@gmail.com>
2025-02-20 08:19:53 +0900

    Add Longest Common Prefix

---
+++ medium/35045_longest_common_prefix.ts
@@ -0,0 +1,76 @@
+/*
+  35045 - Longest Common Prefix
+  -------
+  by Tom Cleary (@thomcleary) #medium
+
+  ### Question
+
+  ### Longest Common Prefix
+
+  Write a type, `LongestCommonPrefix` that returns the longest common prefix string amongst a tuple of strings.
+
+  If there is no common prefix, return an empty string `""`.
+
+  ```ts
+  type Common = LongestCommonPrefix<["flower", "flow", "flight"]>
+  //   ?^ "fl"
+
+  type Uncommon = LongestCommonPrefix<["dog", "racecar", "race"]>
+  //   ?^ ""
+  ```
+  Inspired by [LeetCode 14. Longest Common Prefix](https://leetcode.com/problems/longest-common-prefix/description/)
+
+  > View on GitHub: https://tsch.js.org/35045
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type CharAt<S extends string, N extends number, C extends unknown[] = []> = S extends `${infer First}${infer Rest}`
+    ? C["length"] extends N
+        ? First
+        : CharAt<Rest, N, [...C, unknown]>
+    : undefined;
+type MapCharAt<T extends string[], N extends number> = T extends [infer First extends string, ...infer Rest extends string[]]
+    ? [CharAt<First, N>, ...MapCharAt<Rest, N>]
+    : [];
+type AllSame<T extends unknown[], U = never> = T extends [infer First, ...infer Rest]
+    ? [U] extends [never]
+        ? AllSame<Rest, First>
+        : First extends U
+            ? AllSame<Rest, U>
+            : false
+    : true;
+type LongestCommonPrefixInner<T extends string[], C extends unknown[] = []> = MapCharAt<T, C["length"]> extends infer U extends unknown[]
+    ? AllSame<U> extends true
+        ? U[0] extends string
+            ? `${U[0]}${LongestCommonPrefixInner<T, [...C, unknown]>}`
+            : ""
+        : ""
+    : never;
+type LongestCommonPrefix<T extends string[]> = LongestCommonPrefixInner<T>;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<LongestCommonPrefix<['flower', 'flow', 'flight']>, 'fl'>>,
+  Expect<Equal<LongestCommonPrefix<['dog', 'racecar', 'race']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['', '', '']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['a', '', '']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['', 'a', '']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['', '', 'a']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['a', 'a', '']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['a', '', 'a']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['', 'a', 'a']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['a', 'a', 'a']>, 'a'>>,
+  Expect<Equal<LongestCommonPrefix<['abc', 'abcd', 'abcde']>, 'abc'>>,
+  Expect<Equal<LongestCommonPrefix<[' ', ' ', ' ']>, ' '>>,
+  Expect<Equal<LongestCommonPrefix<['type-challenges', 'type-hero', 'typescript']>, 'type'>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/35045/answer
+  > View solutions: https://tsch.js.org/35045/solutions
+  > More Challenges: https://tsch.js.org
+*/
