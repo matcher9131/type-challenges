a03d2f67e59300281ac6c2846d748a124d35c42c
matcher9131 <matcher9131cd@gmail.com>
2025-02-24 08:44:41 +0900

    Add IsPalindrome

---
+++ hard/4037_is_palindrome.ts
@@ -0,0 +1,53 @@
+/*
+  4037 - IsPalindrome
+  -------
+  by jiangshan (@jiangshanmeta) #hard #string
+
+  ### Question
+
+  Implement type ```IsPalindrome<T>``` to check whether  a string or number is palindrome.
+
+  For example:
+
+  ```typescript
+  IsPalindrome<'abc'> // false
+  IsPalindrome<121> // true
+  ```
+
+  > View on GitHub: https://tsch.js.org/4037
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type StringToCharTuple<S extends string> = S extends `${infer First}${infer Rest}`
+    ? [First, ...StringToCharTuple<Rest>]
+    : [];
+
+type IsPalindromeInner<T extends unknown[]> = T extends [infer First, ...infer Middle, infer Last]
+    ? Equal<First, Last> extends true
+        ? IsPalindromeInner<Middle>
+        : false
+    : true;
+
+type IsPalindrome<T extends string | number> = IsPalindromeInner<StringToCharTuple<`${T}`>>;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<IsPalindrome<'abc'>, false>>,
+  Expect<Equal<IsPalindrome<'b'>, true>>,
+  Expect<Equal<IsPalindrome<'abca'>, false>>,
+  Expect<Equal<IsPalindrome<'abba'>, true>>,
+  Expect<Equal<IsPalindrome<'abcba'>, true>>,
+  Expect<Equal<IsPalindrome<121>, true>>,
+  Expect<Equal<IsPalindrome<2332>, true>>,
+  Expect<Equal<IsPalindrome<19260817>, false>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/4037/answer
+  > View solutions: https://tsch.js.org/4037/solutions
+  > More Challenges: https://tsch.js.org
+*/
